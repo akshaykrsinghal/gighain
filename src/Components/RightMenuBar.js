a94a348f289d5708de5d2fb3c53a1c96eed1ef1f
akshay <akshay.singhal@neosoftmail.com>
2023-05-03 15:34:19 +0530

    docker

--- src/Components/RightMenuBar.js
+++ src/Components/RightMenuBar.js
@@ -37,7 +37,7 @@ function RightMenuBar() {
 
   return (
     <RightWrapper>
-      <TitleWrapper>Gig Execution Module</TitleWrapper>
+      <TitleWrapper>Gig Execution Module New2</TitleWrapper>
       <SubHeading>Allocate Gigs, Track Execution, Track Execution by Giggers</SubHeading>
       <MenuWrapper>
         {
