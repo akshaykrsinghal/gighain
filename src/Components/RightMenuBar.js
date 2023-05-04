56c2bddf57fb029876038f70617b87f4b3aefa1f
akshay <akshay.singhal@neosoftmail.com>
2023-05-04 11:29:09 +0530

    husky-docker

--- src/Components/RightMenuBar.js
+++ src/Components/RightMenuBar.js
@@ -37,7 +37,7 @@ function RightMenuBar() {
 
   return (
     <RightWrapper>
-      <TitleWrapper>Gig Execution Module</TitleWrapper>
+      <TitleWrapper>Gig Execution Module with Docker</TitleWrapper>
       <SubHeading>Allocate Gigs, Track Execution, Track Execution by Giggers</SubHeading>
       <MenuWrapper>
         {
